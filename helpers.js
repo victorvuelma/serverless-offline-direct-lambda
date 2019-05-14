d9f8609b6e8de3b4098fd743da43aff20bb92eb2
Tyler Zey <tyler.zey@equips.com>
2019-05-14 09:18:04 -0500

    update

--- helpers.js
+++ helpers.js
@@ -2,6 +2,8 @@ var AWS = require("aws-sdk");
 
 const getPort = () => "3000";
 const getRegion = () => "us-east-1";
+const getNameOfServerless = () => "equips-app-api";
+const getEnv = () => "dev";
 
 AWS.config.region = getRegion();
 
@@ -9,4 +11,8 @@ const lambda = new AWS.Lambda({
   endpoint: `http://localhost:${getPort()}`
 });
 
+const buildLambdaName = nameOfHandler =>
+  `${getNameOfServerless()}-${getEnv()}-${nameOfHandler}`;
+
 module.exports.lambda = lambda;
+module.exports.buildLambdaName = buildLambdaName;
